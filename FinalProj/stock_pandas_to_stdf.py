c3f7a8d6a0db5c8046a7cd150ae70ee13970b7ef
Iris Yeung <iris@Iriss-Mac.local>
2020-12-03 22:36:36 -0500

    modifed stock_pandas_to_stdf file

--- FinalProj/stock_pandas_to_stdf.py
+++ FinalProj/stock_pandas_to_stdf.py
@@ -13,41 +13,41 @@ import stock_dataframe_to_stdf as sdts
 
 def data_processing(stockdf):
     # simple moving average
-    stockdf['ma:2']
-    stockdf['ma:5']
-    stockdf['ma:2,open']
-    stockdf['ma:5,open']
+    # stockdf['ma:2']
+    # stockdf['ma:5']
+    # stockdf['ma:2,open']
+    # stockdf['ma:5,open']
     # Exponential Moving Average
-    stockdf['ema:5']
+    # stockdf['ema:5']
 
     # Moving Average Convergence Divergence
-    stockdf['macd:2,3']
-    stockdf['macd.s']
-    stockdf['macd.h']
+    # stockdf['macd:2,3']
+    # stockdf['macd.s']
+    # stockdf['macd.h']
     # BOLLinger bands
-    stockdf['boll:2,open']
+    # stockdf['boll:2,open']
     # Raw Stochastic Value
-    stockdf['rsv:2']
+    # stockdf['rsv:2']
     # stochastic oscillator
-    stockdf['kdj.d']
+    # stockdf['kdj.d']
     # Relative Strength Index
-    stockdf['rsi:2']
+    # stockdf['rsi:2']
     # Bull and Bear Index
-    stockdf['bbi']
+    # stockdf['bbi']
     # Lowest of Low Values
-    stockdf['llv:2,low']
-    stockdf['llv:2,close']
-    stockdf['llv:5,low']
-    stockdf['llv:5,close']
-    stockdf['llv:10,low']
-    stockdf['llv:10,close']
+    # stockdf['llv:2,low']
+    # stockdf['llv:2,close']
+    # stockdf['llv:5,low']
+    # stockdf['llv:5,close']
+    # stockdf['llv:10,low']
+    # stockdf['llv:10,close']
     # Highest of High Values
-    stockdf['hhv:2,low']
-    stockdf['hhv:2,close']
-    stockdf['hhv:5,low']
-    stockdf['hhv:5,close']
-    stockdf['hhv:10,low']
-    stockdf['hhv:10,close']
+    # stockdf['hhv:2,low']
+    # stockdf['hhv:2,close']
+    # stockdf['hhv:5,low']
+    # stockdf['hhv:5,close']
+    # stockdf['hhv:10,low']
+    # stockdf['hhv:10,close']
     # bullish/bearish
     stockdf['style:bullish']
     # increase
@@ -67,6 +67,6 @@ if __name__ == "__main__":
     df = sdts.to_df(stockdf)
     print(stockdf.head(5))
     print(stockdf.shape)
-    df.to_excel("./Shijing_try/data_v1.xlsx",engine="xlsxwriter")
+    df.to_excel("./Shijing_try/data_v2.xlsx",engine="xlsxwriter")
 
 
